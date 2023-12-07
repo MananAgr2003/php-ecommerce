a3ffa53bb0711cb871c87fd6db48a12c128c9ffe
Manan Agrawal <agrawalmanan2@gmail.com>
2023-12-07 22:27:52 +0530

    initial commit

---
+++ theme/templates.php
@@ -0,0 +1,440 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Home | E-Shopper</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/price-range.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+  <link href="css/main.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/responsive.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+ 
+
+<?php
+if (isset($_SESSION['gcCart'])){
+  if (@count($_SESSION['gcCart'])>0) {
+    $cart = '<span class="carttxtactive">('.@count($_SESSION['gcCart']) .')</span>';
+  } 
+ 
+} 
+ ?>
+ 
+<script type="text/javascript">
+   
+
+</script>
+</head>
+
+<body style="background-color:white" onload="totalprice()" >
+
+  <!-- Humberger Begin -->
+  <div class="humberger__menu__overlay"></div>
+    <div class="humberger__menu__wrapper">
+        <div class="humberger__menu__logo">
+            <a href="#"><img src="img/logo.png" alt=""></a>
+        </div>
+        <div class="humberger__menu__cart">
+            <ul>
+                <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
+                <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+            </ul>
+            <div class="header__cart__price">item: <span>$150.00</span></div>
+        </div>
+        <div class="humberger__menu__widget">
+            
+            <div class="header__top__right__auth">
+                <a href="#"><i class="fa fa-user"></i> Login</a>
+            </div>
+        </div>
+        <div id="mobile-menu-wrap"></div>
+        <div class="header__top__right__social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-linkedin"></i></a>
+            <a href="#"><i class="fa fa-pinterest-p"></i></a>
+        </div>
+        <div class="humberger__menu__contact">
+            <ul>
+                <li><i class="fa fa-envelope"></i> shop@citymart.com</li>
+                <li>Free Shipping for all Order of $99</li>
+            </ul>
+        </div>
+    </div>
+    <!-- Humberger End -->
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="header__top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="header__top__left">
+                            <ul>
+                                <li><i class="fa fa-envelope"></i> shop@citymart.com</li>
+                                <li>Free Shipping for all Order of $99</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="header__top__right">
+                            <div class="header__top__right__social">
+                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                <a href="#"><i class="fa fa-linkedin"></i></a>
+                                <a href="#"><i class="fa fa-pinterest-p"></i></a>
+                            </div>
+                        
+                            <div class="header__top__right__auth">
+                                <a href="#"><i class="fa fa-user"></i> Login</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="header__logo">
+                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-6" style="padding-top: 40px ;">
+                    <nav class="header__menu">
+                        <ul>
+                            <li class="active"><a href="<?php web_root?>index.php?q=home">Home</a></li>
+                            <li><a href="<?php web_root?>index.php?q=product"> Products </a></li>
+                            <li><a href="<?php web_root?>index.php?q=product">Check Out</a></li>
+                            <li><a href="<?php web_root?>index.php?q=contact">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-lg-3">
+                    <div class="header__cart">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-heart"></i> <span>1</span></a></li>
+                            <li><a href="#"><i class="fa fa-shopping-bag"></i> <span>3</span></a></li>
+                        </ul>
+                        <div class="header__cart__price">item: <span>$150.00</span></div>
+                    </div>
+                </div>
+            </div>
+            <div class="humberger__open">
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+    </header>
+    <!-- Header Section End -->
+
+
+
+          <?php 
+            require_once $content; 
+         ?> 
+
+
+
+
+
+    <footer id="footer"><!--Footer-->
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-2">
+            <div class="companyinfo">
+              <h2><span>e</span>-shopper</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,sed do eiusmod tempor</p>
+            </div>
+          </div>
+          <div class="col-sm-7">
+            <div class="col-sm-6">
+              <div class="video-gallery text-center">
+                <a href="#"> 
+                    <iframe class="iframe-img"  src="https://www.youtube.com/embed/d8N21Q_UN4w"   allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+                </a> 
+              </div>
+            </div>
+            
+            <div class="col-sm-6">
+              <div class="video-gallery text-center">
+                <a href="#"> 
+                   <iframe  class="iframe-img"  src="https://www.youtube.com/embed/aMDC3Da4KIA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> 
+                </a> 
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="address">
+              <img src="images/home/map.png" alt="" />
+              <p>505 S Atlantic Ave Virginia Beach, VA(Virginia)</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="footer-widget">
+      <div class="container">
+        <p>You can modify this section using your own personal information</p>
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="single-widget">
+              <h2>Service</h2>
+              <ul class="nav nav-pills nav-stacked">
+                <li><a href="#">Online Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Order Status</a></li>
+                <li><a href="#">Change Location</a></li>
+                <li><a href="#">FAQ’s</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="single-widget">
+              <h2>Quick Shop</h2>
+              <ul class="nav nav-pills nav-stacked">
+                 <?php 
+                      $mydb->setQuery("SELECT * FROM `tblcategory` LIMIT 6");
+                      $cur = $mydb->loadResultList();
+                     foreach ($cur as $result) {
+                      echo '<li><a href="index.php?q=product&category='.$result->CATEGORIES.'" >'.$result->CATEGORIES.'</a></li>';
+                      }
+                  ?>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="single-widget">
+              <h2>Policies</h2>
+              <ul class="nav nav-pills nav-stacked">
+                <li><a href="#">Terms of Use</a></li>
+                <li><a href="#">Privecy Policy</a></li>
+                <li><a href="https://youtu.be/9hcCHTwW3HA">HR Record System</a></li>
+                <li><a href="https://youtu.be/aMDC3Da4KIA">Point of Sales</a></li>
+                <li><a href="https://youtu.be/d8N21Q_UN4w">Resevation System</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="single-widget">
+              <h2>About Shopper</h2>
+              <ul class="nav nav-pills nav-stacked">
+                <li><a href="#">Company Information</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Store Location</a></li>
+                <li><a href="#">Affillate Program</a></li>
+                <li><a href="#">Copyright</a></li>
+              </ul>
+            </div>
+          </div>
+      
+          
+        </div>
+      </div>
+    </div>
+    
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="row">
+          <p class="pull-left">Modified by <span><a href="https://bit.ly/2LPn9Wu">Janobe Source Code</a></span></p>
+          <p class="pull-right">Designed by <span><a target="_blank" href="http://www.themeum.com">Themeum</a></span></p>
+        </div>
+      </div>
+    </div>
+    
+  </footer><!--/Footer-->
+
+ <!-- modalorder -->
+ <div class="modal fade" id="myOrdered">
+ </div>
+
+
+ <?php include "LogSignModal.php"; ?> 
+<!-- end -->
+ 
+    <!-- jQuery -->
+    <script src="<?php echo web_root; ?>jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo web_root; ?>js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript --> 
+    <!-- DataTables JavaScript -->
+    <script src="<?php echo web_root; ?>js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo web_root; ?>js/dataTables.bootstrap.min.js"></script>
+
+
+<script type="text/javascript" language="javascript" src="<?php echo web_root; ?>js/ekko-lightbox.js"></script> 
+<script type="text/javascript" src="<?php echo web_root; ?>js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+<script type="text/javascript" src="<?php echo web_root; ?>js/locales/bootstrap-datetimepicker.uk.js" charset="UTF-8"></script>
+
+   
+<script src="<?php echo web_root; ?>js/jquery.scrollUp.min.js"></script>
+<script src="<?php echo web_root; ?>js/price-range.js"></script>
+<script src="<?php echo web_root; ?>js/jquery.prettyPhoto.js"></script>
+<script src="<?php echo web_root; ?>js/main.js"></script> 
+
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script type="text/javascript" src="js/gmaps.js"></script>
+  <script src="js/contact.js"></script>
+
+    <!-- Custom Theme JavaScript --> 
+<script type="text/javascript" language="javascript" src="<?php echo web_root; ?>js/janobe.js"></script> 
+ <script type="text/javascript">
+  $(document).on("click", ".proid", function () {
+    // var id = $(this).attr('id');
+      var proid = $(this).data('id')
+    // alert(proid)
+       $(".modal-body #proid").val( proid )
+
+      });
+
+</script>
+ <script>
+    // tooltip demo
+    $('.tooltip-demo').tooltip({
+        selector: "[data-toggle=tooltip]",
+        container: "body"
+    })
+
+    // popover demo
+    $("[data-toggle=popover]")
+        .popover()
+    </script>
+
+
+      <script>
+        $('.carousel').carousel({
+            interval: 5000 //changes the speed
+        })
+    </script>
+
+<script type="text/javascript">
+
+
+$('#date_picker').datetimepicker({
+  format: 'mm/dd/yyyy',
+    language:  'en',
+    weekStart: 1,
+    todayBtn:  1,
+    autoclose: 1,
+    todayHighlight: 1,
+    startView: 2,
+    minView: 2,
+    forceParse: 0
+    });
+
+ 
+ 
+ 
+function validatedate(){ 
+ 
+ 
+
+    var todaysDate = new Date() ;
+
+    var txtime =  document.getElementById('ftime').value
+    // var myDate = new Date(dateme); 
+
+    var tprice = document.getElementById('alltot').value 
+    var BRGY = document.getElementById('BRGY').value
+    var onum = document.getElementById('ORDERNUMBER').value
+
+     
+     var mytime = parseInt(txtime)  ;
+     var todaytime =  todaysDate.getHours()  ;
+       if (txtime==""){
+     alert("You must set the time enable to submit the order.")
+     }else 
+     if (mytime<todaytime){ 
+        alert("Selected time is invalid. Set another time.")
+      }else{
+        window.location = "index.php?page=7&price="+tprice+"&time="+txtime+"&BRGY="+BRGY+"&ordernumber="+onum; 
+      }
+  }
+</script>  
+
+
+    <script type="text/javascript">
+  $('.form_curdate').datetimepicker({
+        language:  'en',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+  $('.form_bdatess').datetimepicker({
+        language:  'en',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+</script>
+<script>
+ 
+
+
+  function checkall(selector)
+  {
+    if(document.getElementById('chkall').checked==true)
+    {
+      var chkelement=document.getElementsByName(selector);
+      for(var i=0;i<chkelement.length;i++)
+      {
+        chkelement.item(i).checked=true;
+      }
+    }
+    else
+    {
+      var chkelement=document.getElementsByName(selector);
+      for(var i=0;i<chkelement.length;i++)
+      {
+        chkelement.item(i).checked=false;
+      }
+    }
+  }
+   function checkNumber(textBox){
+        while (textBox.value.length > 0 && isNaN(textBox.value)) {
+          textBox.value = textBox.value.substring(0, textBox.value.length - 1)
+        }
+        textBox.value = trim(textBox.value);
+      }
+      //
+      function checkText(textBox)
+      {
+        var alphaExp = /^[a-zA-Z]+$/;
+        while (textBox.value.length > 0 && !textBox.value.match(alphaExp)) {
+          textBox.value = textBox.value.substring(0, textBox.value.length - 1)
+        }
+        textBox.value = trim(textBox.value);
+      }
+  
+
+       
+  </script>     
+
+</body>
+</html>
